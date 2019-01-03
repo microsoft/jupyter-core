56f5484033788b848d5a654f1398ffae3a6068c9
Christopher Granade <cgranade@cgranade.com>
2019-01-03 15:36:58 -0800

    Added some more doc comments.

--- src/Extensions/Conversions.cs
+++ src/Extensions/Conversions.cs
@@ -15,6 +15,15 @@ namespace Microsoft.Jupyter.Core
     public static partial class Extensions
     {
 
+        /// <summary>
+        ///      Converts a string containing hexadecimal digits to an array of
+        ///      bytes representing the same data.
+        /// </summary>
+        /// <param name="hex">
+        ///     A string containing an even number of hexadecimal characters
+        ///     (0-f).
+        /// </param>
+        /// <returns>An array of bytes representing the same data.</returns>
         public static byte[] HexToBytes(this string hex)
         {
             var bytes = new byte[hex.Length / 2];
@@ -25,6 +34,10 @@ namespace Microsoft.Jupyter.Core
             return bytes;
         }
 
+        /// <summary>
+        ///     Encapsulates encoded data into an <see cref="EncodedData" />
+        ///     value without metadata.
+        /// </summary>
         public static EncodedData ToEncodedData(this string data) =>
             new EncodedData
             {
@@ -32,6 +45,15 @@ namespace Microsoft.Jupyter.Core
                 Metadata = null
             };
 
+        /// <summary>
+        ///     Encapsulates an execution status as a result object without
+        ///     output. This is useful when an input being executed has
+        ///     completed, but has not produced output; e.g. after a print
+        ///     statement or function.
+        /// </summary>
+        /// <param name="status">
+        ///     The status to be encapsulated as the result of an execution.
+        /// </param>
         public static ExecutionResult ToExecutionResult(this ExecuteStatus status) =>
             new ExecutionResult
             {
@@ -39,6 +61,17 @@ namespace Microsoft.Jupyter.Core
                 Output = null
             };
 
+        /// <summary>
+        ///      Encapsulates a given output as the result of an execution.
+        ///      By default, this method denotes that an execution completed
+        ///      successfully.
+        /// </summary>
+        /// <param name="output">
+        ///      The output from an execution.
+        /// </param>
+        /// <param name="status">
+        ///     The status to be encapsulated as the result of an execution.
+        /// </param>
         public static ExecutionResult ToExecutionResult(this object output, ExecuteStatus status = ExecuteStatus.Ok) =>
             new ExecutionResult
             {
