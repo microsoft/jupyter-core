60139ffe8b92b03b460604003101139ea73dd7aa
Christopher Granade <cgranade@cgranade.com>
2019-01-03 17:32:39 -0800

    Described kernel contexts.

--- src/Data/KernelContext.cs
+++ src/Data/KernelContext.cs
@@ -9,11 +9,23 @@ using Newtonsoft.Json;
 
 namespace Microsoft.Jupyter.Core
 {
+    /// <summary>
+    ///      Describes all information needed to identify a kernel at runtime,
+    ///      including how to connect to clients, and a description of the
+    ///      kernel's supported language.
+    /// </summary>
     public class KernelContext
     {
         public ConnectionInfo ConnectionInfo { get; private set; }
         public KernelProperties Properties { get; set; }
 
+        /// <summary>
+        ///      Populates the context using a connection file, typically
+        ///      provided by Jupyter when instantiating a kernel.
+        /// </summary>
+        /// <param name="connectionFile">
+        ///     A path to the connection file to be loaded.
+        /// </param>
         public void LoadConnectionFile(string connectionFile)
         {
             this.ConnectionInfo = JsonConvert.DeserializeObject<ConnectionInfo>(File.ReadAllText(connectionFile));
