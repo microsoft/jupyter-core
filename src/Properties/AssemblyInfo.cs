9b12ac60ce8c081d9702b00880ee91631bd4a239
Andres Paz <anpaz@microsoft.com>
2019-01-09 20:01:01 -0800

    Adding DelaySign

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Microsoft.Jupyter.Core.Tests")]
+[assembly: InternalsVisibleTo("Microsoft.Jupyter.Core.Tests" + SigningConstants.PUBLIC_KEY)]
