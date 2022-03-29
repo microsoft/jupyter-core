0a80507715e80a000b7425a5c1da92722f5756d0
IsraelMiles <israelmiles@microsoft.com>
2022-03-29 14:09:26 -0700

    initialize metadata to empty string for busy message

--- src/Extensions/Extensions.cs
+++ src/Extensions/Extensions.cs
@@ -52,7 +52,8 @@ namespace Microsoft.Jupyter.Core
                     Content = new KernelStatusContent
                     {
                         ExecutionState = state
-                    }
+                    },
+                    Metadata = ""
                 }.AsReplyTo(message)
             );
         }
